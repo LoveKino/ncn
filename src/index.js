0ef5bb26ea57b00b4f8ce5d7da0aef0689dd549a
loveKino <842914439@qq.com>
2016-07-22 15:58:16 +0800

    style object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+let {
+    isString, isObject, isNode, isArray
+} = require('./util');
+
+let parseAttribute = require('./parseAttribute');
+
 const svgNS = 'http://www.w3.org/2000/svg';
 
 let cn = (create) => (...args) => {
@@ -26,26 +32,22 @@ let cn = (create) => (...args) => {
         node = create(tagName);
     }
 
-    attributes = parseAttribute(attributes);
-
     let next = args.shift();
+
+    let nextAttr = {};
+
     if (isArray(next) || isString(next)) {
         childExp = next;
     } else if (isObject(next)) {
-        attributes = merge(attributes, next);
+        nextAttr = next;
         childExp = args.shift() || [];
     }
+
+    attributes = parseAttribute(attributes, nextAttr);
 
     applyNode(node, attributes, childExp);
 
     return node;
-};
-
-let merge = (map1 = {}, map2) => {
-    for (let name in map2) {
-        map1[name] = map2[name];
-    }
-    return map1;
 };
 
 let splitTagNameAttribute = (str = '') => {
@@ -59,41 +61,6 @@ let splitTagNameAttribute = (str = '') => {
     }
 };
 
-const ITEM_REG = /([\w-]+)\s*=\s*(([\w-]+)|('.*?')|(".*?"))/;
-
-// TODO better key=value grammer
-let parseAttribute = (attributes) => {
-    // key=value key=value
-    // value='abc' value=true value=123 value="def"
-    if (isString(attributes)) {
-        let str = attributes.trim(),
-            kvs = [];
-
-        let stop = false;
-        while (!stop) {
-            let newstr = str.replace(ITEM_REG, (matchStr, $1, $2) => {
-                kvs.push([$1, $2]);
-                return '';
-            }).trim();
-            if (newstr === str) {
-                stop = true;
-            }
-            str = newstr;
-        }
-
-        attributes = {};
-        for (let i = 0; i < kvs.length; i++) {
-            let [key, value] = kvs[i];
-            if (value[0] === '\'' && value[value.length - 1] === '\'' ||
-                value[0] === '"' && value[value.length - 1] === '"') {
-                value = value.substring(1, value.length - 1);
-            }
-            attributes[key] = value;
-        }
-    }
-
-    return attributes;
-};
 
 let applyNode = (node, attributes, childExp) => {
     setAttributes(node, attributes);
@@ -104,26 +71,8 @@ let applyNode = (node, attributes, childExp) => {
 let setAttributes = (node, attributes) => {
     for (let name in attributes) {
         let attr = attributes[name];
-        if (name === 'style') {
-            attr = getStyleString(attr);
-        }
         node.setAttribute(name, attr);
     }
-};
-
-let getStyleString = (attr = '') => {
-    if (isString(attr)) {
-        return attr;
-    }
-
-    if (!isObject(attr)) {
-        throw new TypeError(`Expect object for style object, but got ${attr}`);
-    }
-    let style = '';
-    for (let key in attr) {
-        style = `${style};${key}: ${attr[key]}`;
-    }
-    return style;
 };
 
 let appendChildExp = (node, childExp) => {
@@ -138,19 +87,6 @@ let appendChildExp = (node, childExp) => {
         node.textContent = childExp.toString();
     }
 };
-
-let isNode = (o) => {
-    return (
-        typeof Node === 'object' ? o instanceof Node :
-        o && typeof o === 'object' && typeof o.nodeType === 'number' && typeof o.nodeName === 'string'
-    );
-};
-
-let isArray = v => v && typeof v === 'object' && typeof v.length === 'number';
-
-let isObject = v => v && typeof v === 'object';
-
-let isString = v => typeof v === 'string';
 
 module.exports = {
     svgn: cn((tagName) => document.createElementNS(svgNS, tagName)),
---
+++ src/parseAttribute.js
@@ -0,0 +1,73 @@
+'use strict';
+
+let {
+    isString, isObject, merge
+} = require('./util');
+
+const ITEM_REG = /([\w-]+)\s*=\s*(([\w-]+)|('.*?')|(".*?"))/;
+
+// TODO better key=value grammer
+// TODO refactor with grammerL: class grammer, id grammer, refer some popular grammer
+let parseAttribute = (attributes, nextAttr) => {
+    // key=value key=value
+    // value='abc' value=true value=123 value="def"
+    if (isString(attributes)) {
+        let str = attributes.trim(),
+            kvs = [];
+
+        let stop = false;
+        while (!stop) {
+            let newstr = str.replace(ITEM_REG, (matchStr, $1, $2) => {
+                kvs.push([$1, $2]);
+                return '';
+            }).trim();
+            if (newstr === str) {
+                stop = true;
+            }
+            str = newstr;
+        }
+
+        attributes = {};
+        for (let i = 0; i < kvs.length; i++) {
+            let [key, value] = kvs[i];
+            if (value[0] === '\'' && value[value.length - 1] === '\'' ||
+                value[0] === '"' && value[value.length - 1] === '"') {
+                value = value.substring(1, value.length - 1);
+            }
+            attributes[key] = value;
+        }
+    }
+    // merge
+    attributes = merge(attributes, nextAttr);
+
+    if (attributes.style) {
+        attributes.style = getStyleString(attributes.style);
+    }
+
+    return attributes;
+};
+
+let getStyleString = (attr = '') => {
+    if (isString(attr)) {
+        return attr;
+    }
+
+    if (!isObject(attr)) {
+        throw new TypeError(`Expect object for style object, but got ${attr}`);
+    }
+    let style = '';
+    for (let key in attr) {
+        let value = attr[key];
+        key = convertStyleKey(key);
+        style = `${style};${key}: ${value}`;
+    }
+    return style;
+};
+
+let convertStyleKey = (key) => {
+    return key.replace(/[A-Z]/, (letter) => {
+        return `-${letter.toLowerCase()}`;
+    });
+};
+
+module.exports = parseAttribute;
